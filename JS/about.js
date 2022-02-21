e25c3e8d45da9e0b1e62fd040eb377bbdafd9b86
Adilet24 <96615575+Adilet24@users.noreply.github.com>
2022-02-21 18:24:11 +0600

    Add files via upload

---
+++ JS/about.js
@@ -0,0 +1,15 @@
+function onmouseoverNavPrice(){
+  document.getElementById("price").style.fontWeight = "bold";
+}
+
+function onmouseoutNavPrice(){
+  document.getElementById("price").style.fontWeight = "normal";
+}
+
+function onmouseoverNavAbout(){
+  document.getElementById("about").style.fontWeight = "bold";
+}
+
+function onmouseoutNavAbout(){
+  document.getElementById("about").style.fontWeight = "normal";
+}
---
+++ JS/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#sale").text("Only until 28.02. From 5825 tenge/month");
+
+});
+function membershipF(){
+  var phone = prompt("Please write your number here for consultation");
+}
+
+function onmouseoverNavPrice(){
+  document.getElementById("price").style.fontWeight = "bold";
+}
+
+function onmouseoutNavPrice(){
+  document.getElementById("price").style.fontWeight = "normal";
+}
+
+function onmouseoverNavAbout(){
+  document.getElementById("about").style.fontWeight = "bold";
+}
+
+function onmouseoutNavAbout(){
+  document.getElementById("about").style.fontWeight = "normal";
+}
